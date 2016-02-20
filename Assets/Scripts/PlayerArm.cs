6ea129408ec3f7eae6a00688ecfe31e9a585dc0b
Raymond Yang <ray.yang@utexas.edu>
2016-02-20 14:18:42 -0600

    Initial Player Scripts

---
+++ Assets/Scripts/PlayerArm.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerArm : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
